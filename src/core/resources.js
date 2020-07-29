4cc17b981a2527e1b903068fc9f0950e95f36d7c
markbello <mcbello51286@gmail.com>
2020-07-29 19:51:49 -0400

    add dogs resource

--- src/core/resources.js
+++ src/core/resources.js
@@ -2,4 +2,7 @@ export default {
   nicholas: {
     fetch: () => ({ url: 'http://localhost:4000/nicholas' }),
   },
+  dogs: {
+    fetch: () => ({ url: 'https://dog.ceo/api/breeds/image/random' }),
+  },
 };
